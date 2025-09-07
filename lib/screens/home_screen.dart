e8ac3e45c371f659a3d93e4207cd8993170ce0cd
msemega781-jpg <msemega781@gmail.com>
2025-09-07 20:23:01 +0000

    Create home_screen.dart

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
+import 'screens/settings_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'LightPC Emu Flutter',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const HomeScreen(),
+        '/settings': (context) => const SettingsScreen(),
+      },
+    );
+  }
+}
